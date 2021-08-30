aa84e39b2aa0203a09722aa2f97adb82f6728dda
uabj8000 <uabj8000@gmail.com>
2021-08-30 17:01:30 -0400

    root

---
+++ src/main/java/com/devsuperior/dsorders/DsordersApiApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsordersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsordersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dsorders/DsordersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsordersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
