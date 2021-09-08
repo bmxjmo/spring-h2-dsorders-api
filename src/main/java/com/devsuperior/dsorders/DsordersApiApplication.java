ca4e495d80d862e8bb3ff04738f4b2ba42081e22
uabj8000 <uabj8000@gmail.com>
2021-09-08 18:47:40 -0400

    feat app, fix CommandLineRunner, add orders

--- src/main/java/com/devsuperior/dsorders/DsordersApiApplication.java
+++ src/main/java/com/devsuperior/dsorders/DsordersApiApplication.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsorders;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.dsorders.entities.Client;
+import com.devsuperior.dsorders.entities.Order;
+import com.devsuperior.dsorders.entities.OrderStatus;
 import com.devsuperior.dsorders.entities.Product;
 import com.devsuperior.dsorders.repositories.ClientRepository;
+import com.devsuperior.dsorders.repositories.OrderRepository;
 import com.devsuperior.dsorders.repositories.ProductRepository;
 
 @SpringBootApplication
@@ -20,6 +24,9 @@ public class DsordersApiApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DsordersApiApplication.class, args);
@@ -38,6 +45,11 @@ public class DsordersApiApplication implements CommandLineRunner {
 		Product p3 = new Product(null, "PC", 1200.0);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Order o1 = new Order(null, Instant.parse("2021-04-18T11:25:09Z"), OrderStatus.PAID, c1);
+		Order o2 = new Order(null, Instant.parse("2021-04-20T13:30:00Z"), OrderStatus.CANCELED, c1);
+		
+		orderRepository.saveAll(Arrays.asList(o1, o2));
 		
 	}
 
