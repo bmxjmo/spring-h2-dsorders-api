14c4c1da4311e4f6050aa362cfbe89b22a09cd15
uabj8000 <uabj8000@gmail.com>
2021-09-09 12:51:18 -0400

    feat app, add OrderController, endpoint for tests

---
+++ src/main/java/com/devsuperior/dsorders/OrderController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsorders;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsorders.entities.Order;
+import com.devsuperior.dsorders.repositories.OrderRepository;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+
+	@Autowired
+	private OrderRepository repository;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
+		Order obj = repository.findById(id).get();
+		return ResponseEntity.ok(obj);
+	}
+	
+}
